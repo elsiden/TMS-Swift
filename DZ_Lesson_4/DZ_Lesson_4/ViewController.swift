6e58af6522c232ef61036ba847bd64b3b1b30250
Ivan <v41new@gmail.com>
2021-06-22 21:58:27 +0300

    DZ4 with Dop

--- DZ_Lesson_4/DZ_Lesson_4/ViewController.swift
+++ DZ_Lesson_4/DZ_Lesson_4/ViewController.swift
@@ -37,6 +37,45 @@ func calcAndPrintInfoCredit(where n: Float, and y: Int, and p: Float) { // ум�
     print("Summ of credit: \(n)\nPeriod (year): \(y)\nPercent: \(p)\nMonthly: \(m)\nTotal summ: \(s)")
 }
 
+func findMinMax(where a: Int, and b: Int, and c: Int) -> (min: Int, max: Int) {
+    var min = (a + b - abs(a - b)) / 2
+    min = (min + c - abs(min - c)) / 2
+    var max = (a + b + abs(a - b)) / 2
+    max = (max + c + abs(max - c)) / 2
+    return (min, max)
+}
+
+func swap(where num1: inout Int, and num2: inout Int) {
+    let c = num1
+    num1 = num2
+    num2 = c
+}
+
+func swap(where num1: inout String, and num2: inout String) {
+    let c = num1
+    num1 = num2
+    num2 = c
+}
+
+func swap(where num1: inout Double, and num2: inout Double) {
+    let c = num1
+    num1 = num2
+    num2 = c
+}
+
+func mealInfo(where breakfast: Int) {
+//    let n = 10
+    let protein = 0.4
+    let vitA = 0.25
+    let vitC = 0.35
+    
+    let ostProt = String(format: "%g", 1 - protein * Double(breakfast))
+    let ostA = String(format: "%g", 1 - vitA * Double(breakfast))
+    let ostC = String(format: "%g", 1 - vitC * Double(breakfast))
+    
+    print(ostProt, ostA, ostC)
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -46,23 +85,59 @@ class ViewController: UIViewController {
         var workerIvan: WorkerInfo = ("Ivan", 21, 50000.5)
         var workerVlad: WorkerInfo = ("Vlad", 35, 5000.75)
         var workerMaksim: WorkerInfo = ("Maksim", 42, 30000.9)
-        
+
         print("=== Task 1 ===\n")
-        
+
         calcAndPrintSallary(where: &workerIvan)
         calcAndPrintSallary(where: &workerVlad)
         calcAndPrintSallary(where: &workerMaksim)
-        
+
         print("\n=== Task 2 ===\n")
-        
+
         printAverage(where: 1, and: 2.5, and: 0.5)
-        
+
         print("\n=== Task 3 ===\n")
-        
+
         let n: Float = 1000000
         let y: Int = 10
         let p: Float = 12
         calcAndPrintInfoCredit(where: n, and: y, and: p / 100)
+        
+        print("\n=== Task 1 Dop ===\n")
+
+        let a = 20
+        let b = 25
+        let c = 15
+        let minMax = findMinMax(where: a, and: b, and: c)
+        print(minMax)
+
+        print("\n=== Task 2 Dop ===\n")
+
+        var a1 = 10
+        var b1 = 15
+        swap(where: &a1, and: &b1)
+        print(a1, b1)
+
+        print("\n=== Task 3 Dop ===\n")
+
+        var a2 = "12"
+        var b2 = "34"
+        swap(where: &a2, and: &b2)
+        print(a2, b2)
+
+        var a3 = 12.2
+        var b3 = 9.2
+        swap(where: &a3, and: &b3)
+        print(a3, b3)
+        
+        print("\n=== Task 4 Dop ===\n")
+        
+        let breakfast = 2
+        mealInfo(where: breakfast)
+        
+        
+        
+        
         
     }
 
