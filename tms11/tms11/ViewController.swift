01c79aa93dee30a57940c59a7ee546f7650eed43
elsiden <zealj@mail.ru>
2021-07-19 19:01:54 +0300

    ls11

---
+++ tms11/tms11/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  tms11
+//
+//  Created by Ivan Klishevich on 15.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let sizeScreen = UIScreen.main.bounds.size // size of screen
+    }
+}
+
